3a355e8b6a9a767dc3acb1f4e6fbf68583edb4fd
Kadir Ozyurt <k.ozyurt@nexum.com.tr>
2023-04-11 16:10:27 +0300

    revize

--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import InnerPageHeader from "@/components/page/InnerPageHeader";
 import Free from "@/components/pricing/Free";
 import Pro from "@/components/pricing/Pro";
 import Premium from "@/components/pricing/Premium";
+import { useRouter } from "next/router";
+import en from "locales/en";
+import tr from "locales/tr";
+
+
 const Pricing = () => {
+
+  const router = useRouter();
+  const { locale, locales, defaultLocale } = router;
+  const t = locale === "en" ? en : tr;
+  
   return (
     <>
       <InnerPageHeader title={`${t.SelectYour}`} description={`${t.SelectYour}`}
